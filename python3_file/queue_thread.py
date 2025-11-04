4ecaf69ce65defb204ccc1b341036ac7f02f841a
HeyuPan <panyu0905@gmail.com>
2025-11-05 01:57:42 +0800

    myPythontest02

--- python3_file/queue_thread.py
+++ python3_file/queue_thread.py
@@ -4,26 +4,26 @@ import  queue
 
 q  = queue.Queue()
 
-def  Producer():
+def  producer():
     n = 0
     while n<5:
         n += 1
         q.put(n)
-        print('Producer has created %s\n'  % n)
+        print('producer has created %s\n'  % n)
 time.sleep(0.2)
        
-def  Consumer():
-    count = 0;
+def  consumer():
+    count = 0
     while count<5:
         count += 1
         data = q.get()
-        print('Consumer has used % s\n'  % data)
+        print('consumer has used % s\n'  % data)
     time.sleep(0.2)
         
 
 if  __name__ == '__main__' :
-    p = threading.Thread(target=Producer, name=' ')
-    c = threading.Thread(target=Consumer, name=' ')
+    p = threading.Thread(target=producer, name=' ')
+    c = threading.Thread(target=consumer, name=' ')
     p.start()
     
     c.start()
